d79d68581eda5f40a20e2eb7d05498177cc905d3
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-04-21 16:39:07 +0530

    📦 Import fixes

--- builder/src/gbpkg/generator.py
+++ builder/src/gbpkg/generator.py
@@ -4,11 +4,11 @@
 from pathlib import Path
 from typing import Any, Dict, NamedTuple
 
-from bxpkg.constants import AUTHOR, URL
-from bxpkg.symlinks import add_missing_xcursor
 from clickgen.builders import WindowsCursor, XCursor
 from clickgen.core import CursorAlias
 from clickgen.packagers import WindowsPackager, XPackager
+from gbpkg.constants import AUTHOR, URL
+from gbpkg.symlinks import add_missing_xcursor
 
 
 class Info(NamedTuple):
